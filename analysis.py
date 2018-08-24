265959b5ee056f2209e121f58977a2014c5d9b96
Lorenzo Meninato <lorenzomeninato@gmail.com>
2018-08-24 18:18:32 -0400

    Need to fix geopy. Stops working after many requests

--- analysis.py
+++ analysis.py
@@ -16,21 +16,26 @@ df = pd.read_csv('GFM_data.csv', sep = '\t')
 df = df.drop_duplicates()
 df = df.drop(df.columns[0], axis=1)
 df.columns = headers
+df = df.reset_index(drop=True)
 
 geolocator = Nominatim(user_agent="test")
-location = geolocator.geocode("Mapleton, UT")
-
-latitude, longitude = location.latitude, location.longitude
-
-df['Latitude'] = Nominatim(user_agent="test").geocode(df.Location.astype(str)).latitude
-
-df.apply(lambda row: Nominatim(user_agent="test").geocode(row.Location.astype(str)).latitude)
 
 latitudes = np.array([])
 longitudes = np.array([])
 
 for i in range(len(df)):
-    latitude = geolocator.geocode(df['Location'][i]).latitude
-    longitude = geolocator.geocode(df['Location'][i]).longitude
+    try:
+        latitude = geolocator.geocode(df['Location'][i]).latitude
+        longitude = geolocator.geocode(df['Location'][i]).longitude
+    except:
+        latitude = np.nan
+        longitude = np.nan
+    print("Getting latitude %s" %(i+1))
     latitudes = np.append(latitudes, latitude)
+    print("Getting longitude %s" %(i+1))
     longitudes = np.append(longitudes, longitude)
+    
+df['Latitude'] = latitudes
+df['Longitude'] = longitudes
+
+df.to_csv('GFM_data.csv', sep = '\t')
