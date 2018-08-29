c2276efa331b54656ea2460c83fb44bcbb926ff0
Lorenzo Meninato <lorenzomeninato@gmail.com>
2018-08-28 21:56:59 -0400

    Query geopy until finished
    sorry geopy

--- analysis.py
+++ analysis.py
@@ -8,6 +8,7 @@ Created on Thu Aug 23 23:40:56 2018
 from geopy.geocoders import Nominatim
 import pandas as pd
 import numpy as np
+from time import sleep
 
 headers = ["Url", "Category","Position", "Title", "Location","Amount_Raised", "Goal", "Number_of_Donators", "Length_of_Fundraising", "FB_Shares", "GFM_hearts", "Text"]
 
@@ -18,23 +19,35 @@ df = df.drop(df.columns[0], axis=1)
 df.columns = headers
 df = df.reset_index(drop=True)
 
-geolocator = Nominatim(user_agent="test")
+geolocator = Nominatim(user_agent="test2")
 
 latitudes = np.array([])
 longitudes = np.array([])
 
-for i in range(len(df)):
-    try:
-        latitude = geolocator.geocode(df['Location'][i]).latitude
-        longitude = geolocator.geocode(df['Location'][i]).longitude
-    except:
-        latitude = np.nan
-        longitude = np.nan
-    print("Getting latitude %s" %(i+1))
-    latitudes = np.append(latitudes, latitude)
-    print("Getting longitude %s" %(i+1))
-    longitudes = np.append(longitudes, longitude)
+start = 423
+while start < len(df):
+    for i in range(start,len(df)):
+        try:
+            latitude = geolocator.geocode(df['Location'][i]).latitude
+            longitude = geolocator.geocode(df['Location'][i]).longitude
+        except:
+            sleep(60)
+            latitude = geolocator.geocode(df['Location'][i]).latitude
+            longitude = geolocator.geocode(df['Location'][i]).longitude
+        print("Getting latitude %s" %(i+1))
+        latitudes = np.append(latitudes, latitude)
+        print("Getting longitude %s" %(i+1))
+        longitudes = np.append(longitudes, longitude)
     
+    np.place(latitudes, latitudes == '', -10)
+    latitudes = latitudes.astype(np.float)    
+    first_fail = np.array((latitudes < 0).nonzero()).min()
+    
+    start = start + first_fail
+
+#output = latitudes[0:(first_fail-1)]
+
+
 df['Latitude'] = latitudes
 df['Longitude'] = longitudes
 
